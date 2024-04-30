2d0595d4c92d721bb5b9c63cb69df42fb545ac69
jeffinjude <jeffinjude@gmail.com>
2024-04-30 21:55:48 +0530

    eureka server initial commit

---
+++ src/test/java/com/jeffinjude/eurekaserver/EcommEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffinjude.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
