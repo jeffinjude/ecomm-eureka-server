2277b1cd78d87eccda6ceeffe4b61cfa2bd88e5a
jeffinjude <jeffinjude@gmail.com>
2024-05-08 09:59:29 +0530

    adding docker and k8s config

--- src/test/java/com/jeffinjude/eurekaserver/EcommEurekaServerApplicationTests.java
+++ src/test/java/com/jeffinjude/eurekaserver/EcommEurekaServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EcommEurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
